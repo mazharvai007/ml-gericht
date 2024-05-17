69d0d03eeb4cf7bc3206ad2eb6eaf96ab30dd814
mazhar <swopnil420@gmail.com>
2024-05-18 04:01:22 +0600

    Added container file of the theme

---
+++ src/container/IntroVideo/IntroVideo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './introvideo.css';
+
+const IntroVideo = () => {
+	return <div>IntroVideo</div>;
+};
+
+export default IntroVideo;
---
+++ src/container/Menu/Meal.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './meal.css';
+
+const Meal = () => {
+	return <div>Meal</div>;
+};
+
+export default Meal;
