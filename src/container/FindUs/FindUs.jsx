cb43ab6a0711b48ed02802a11b276c8258bd5dc4
mazhar <swopnil420@gmail.com>
2024-05-21 09:57:25 +0600

    Completed the introvideo and awards area

--- src/container/FindUs/FindUs.jsx
+++ src/container/FindUs/FindUs.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './findUs.css';
+import imgSrc from '../../constants/imgSrc';
+import { Heading } from '../../components';
 
 const FindUs = () => {
-	return <div>FindUs</div>;
+	return (
+		<div className="app__findUs app__bg section__padding flex__center">
+			<div className="app__findUs-contentWrap">
+				<Heading subTitle="Contact" title="Find Us" />
+				<div className="app__findUs-content__findUsInfo"></div>
+			</div>
+
+			<div className="app__findUs-findUsWorld">
+				<img src={imgSrc.findUs} alt="findUs" />
+			</div>
+		</div>
+	);
 };
 
 export default FindUs;
